23a62c8aeed508fc86e37c07b4bd466c0f418891
harshv9988 <harshv9988@gmail.com>
2020-10-22 13:57:02 +0530

    scree list basic layout ready

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -5,7 +5,23 @@ const Screen = () =>{
     return (
         <div className="screen">
             <div className="display-screen">
-
+                <div className="list-container">
+                <div className="title">
+                    IPOD.js
+                </div>
+                    <div className="coverflow">
+                        coverflow
+                    </div>
+                    <div className="music">
+                        Music
+                    </div>
+                    <div className="games">
+                        Games
+                    </div>
+                    <div className="setting">
+                        Setting
+                    </div>
+                </div>
             </div>
         </div>
     );
