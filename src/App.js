1836b88f2021c5adebc6f6daf3f24d077533a9a8
harshv9988 <harshv9988@gmail.com>
2020-10-24 08:40:20 +0530

    rotation changing active class

--- src/App.js
+++ src/App.js
@@ -20,15 +20,106 @@ class App extends React.Component  {
   }
 
   rotateClick = () => {
-    let angle = 0;
+    var angle = 0;
     var target = document.getElementById('rotatediv');
     var region = new ZingTouch.Region(target);
+    const self = this;
+    var counter_clock = 0;
 
     region.bind(target, 'rotate', function(e) {
-      console.log('rotate effect');
-    });
+      // console.log('rotate effect');
+      angle = e.detail.distanceFromLast;
+      // console.log(angle);
+      if(angle>0){
+          if(counter_clock>=0 && counter_clock<10){
+              self.setState({
+                  coverflow : true,
+                  music : false,
+                  games :false,
+                  setting : false,
+            })
+          }
+          else if(counter_clock>=10 && counter_clock<20){
+            self.setState({
+                coverflow : false,
+                music : true,
+                games :false,
+                setting : false,
+            })
+          }
+          else if(counter_clock>=20 && counter_clock<30){
+            self.setState({
+                coverflow : false,
+                music : false,
+                games :true,
+                setting : false,
+            })
+          }
+          else if(counter_clock>=30 && counter_clock<40){
+            self.setState({
+              coverflow : false,
+              music : false,
+              games :false,
+              setting : true,
+          })
+          }    
+          
+          if(counter_clock>40){
+            counter_clock=40;
+          }
+          else{
+            counter_clock = counter_clock+1;
+          }
+      }
 
-  }
+
+      else if(angle<0){
+
+
+        if(counter_clock>0 && counter_clock<=10){
+          self.setState({
+              coverflow : true,
+              music : false,
+              games :false,
+              setting : false,
+        })
+      }
+      else if(counter_clock>10 && counter_clock<=20){
+        self.setState({
+            coverflow : false,
+            music : true,
+            games :false,
+            setting : false,
+        })
+      }
+      else if(counter_clock>20 && counter_clock<=30){
+        self.setState({
+            coverflow : false,
+            music : false,
+            games :true,
+            setting : false,
+        })
+      }
+      else if(counter_clock>30 && counter_clock<=40){
+        self.setState({
+          coverflow : false,
+          music : false,
+          games :false,
+          setting : true,
+        })
+      } 
+      
+      if(counter_clock<0){
+        counter_clock = 0;
+      }
+      else{
+        counter_clock = counter_clock-1;
+      }
+
+    }
+});
+
+}
 
  render(){
    const {coverflow, music, games, setting} = this.state;
