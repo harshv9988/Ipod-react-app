71180f2c38beb2164b2a5b329240441cac18031b
harshv9988 <harshv9988@gmail.com>
2020-10-24 20:33:12 +0530

    changes angle rotation

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends React.Component  {
         if(self.state.musiclist==false){
 
               if(angle>0){
-                if(counter_clock>=0 && counter_clock<10){
+                if(counter_clock>=0 && counter_clock<5){
                     self.setState({
                         coverflow : true,
                         music : false,
@@ -49,7 +49,7 @@ class App extends React.Component  {
                         setting : false,
                   })
                 }
-                else if(counter_clock>=10 && counter_clock<20){
+                else if(counter_clock>=5 && counter_clock<10){
                   self.setState({
                       coverflow : false,
                       music : true,
@@ -57,7 +57,7 @@ class App extends React.Component  {
                       setting : false,
                   })
                 }
-                else if(counter_clock>=20 && counter_clock<30){
+                else if(counter_clock>=10 && counter_clock<15){
                   self.setState({
                       coverflow : false,
                       music : false,
@@ -65,7 +65,7 @@ class App extends React.Component  {
                       setting : false,
                   })
                 }
-                else if(counter_clock>=30 && counter_clock<40){
+                else if(counter_clock>=15 && counter_clock<20){
                   self.setState({
                     coverflow : false,
                     music : false,
@@ -74,8 +74,8 @@ class App extends React.Component  {
                 })
                 }    
                 
-                if(counter_clock>40){
-                  counter_clock=40;
+                if(counter_clock>20){
+                  counter_clock=20;
                 }
                 else{
                   counter_clock = counter_clock+1;
@@ -86,7 +86,7 @@ class App extends React.Component  {
             else if(angle<0){
       
       
-              if(counter_clock>0 && counter_clock<=10){
+              if(counter_clock>0 && counter_clock<=5){
                 self.setState({
                     coverflow : true,
                     music : false,
@@ -94,7 +94,7 @@ class App extends React.Component  {
                     setting : false,
               })
             }
-            else if(counter_clock>10 && counter_clock<=20){
+            else if(counter_clock>5 && counter_clock<=10){
               self.setState({
                   coverflow : false,
                   music : true,
@@ -102,7 +102,7 @@ class App extends React.Component  {
                   setting : false,
               })
             }
-            else if(counter_clock>20 && counter_clock<=30){
+            else if(counter_clock>10 && counter_clock<=15){
               self.setState({
                   coverflow : false,
                   music : false,
@@ -110,7 +110,7 @@ class App extends React.Component  {
                   setting : false,
               })
             }
-            else if(counter_clock>30 && counter_clock<=40){
+            else if(counter_clock>15 && counter_clock<=20){
               self.setState({
                 coverflow : false,
                 music : false,
@@ -132,34 +132,34 @@ class App extends React.Component  {
 
       else{
           if(angle>0){
-            if(counter2>=0 && counter2<10){
+            if(counter2>=0 && counter2<5){
                 self.setState({
                     artist : true,
                     songs : false,
               })
             }
-            else if(counter2>=10 && counter2<20){
+            else if(counter2>=5 && counter2<10){
               self.setState({
                   artist : false,
                   songs : true,
               })
             }
 
-            if(counter2>20){
-              counter2=20;
+            if(counter2>10){
+              counter2=10;
             }
             else{
               counter2 = counter2+1;
             }
         }
         else if(angle<0){
-              if(counter2>0 && counter2<=10){
+              if(counter2>0 && counter2<=5){
                 self.setState({
                     artist : true,
                     songs : false,
               })
             }
-            else if(counter2>10 && counter2<=20){
+            else if(counter2>5 && counter2<=10){
               self.setState({
                   artist : false,
                   songs : true,
