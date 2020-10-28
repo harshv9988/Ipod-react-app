5358c6d0db47768fa710df8d5e103d23d6e01772
harshv9988 <harshv9988@gmail.com>
2020-10-28 14:12:04 +0530

    added setting screen styling

--- src/components/SettingScreen.js
+++ src/components/SettingScreen.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
+import reacticon from "../assets/react-icon.png";
 
 const SettingScreen = (props) => {
-    const {settingscreen} = props;
-    return(
-
-        <div className={`${settingscreen ? "setting-screen" : "hide"}`}>
-          setting
+  const { settingscreen } = props;
+  return (
+    <div className={`${settingscreen ? "setting-screen" : "hide"}`}>
+      <div className="display-screen black-display">
+        <div className="tool">
+          <img src={reacticon} alt="reacticon" />
         </div>
-
-    );
-
-
-}
+        <div className="author">Made by Harsh Verma</div>
+      </div>
+    </div>
+  );
+};
 export default SettingScreen;
