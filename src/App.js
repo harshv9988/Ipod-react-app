8ad98a093aef7fc2c6d99b3388e32f32aaeb820c
harshv9988 <harshv9988@gmail.com>
2020-10-28 12:52:03 +0530

    counter changes

--- src/App.js
+++ src/App.js
@@ -40,156 +40,143 @@ class App extends React.Component  {
   }
 
   rotateClick = () => {
-    var angle = 0;
-    var target = document.getElementById('rotatediv');
-    var region = new ZingTouch.Region(target);
-    const self = this;
-    var counter_clock = 0;
+    var counter = 0;
     var counter2 = 0;
+    const self = this;
 
-    region.bind(target, 'rotate', function(e) {
-     
-        angle = e.detail.distanceFromLast;
-      
-        if(self.state.musiclist==false){
+    var target = document.getElementById('rotatediv');
+    var activeRegion = ZingTouch.Region(target);
 
-              if(angle>0){
-                if(counter_clock>=0 && counter_clock<5){
-                    self.setState({
-                        coverflow : true,
-                        music : false,
-                        games :false,
-                        setting : false,
-                  })
-                }
-                else if(counter_clock>=5 && counter_clock<10){
-                  self.setState({
-                      coverflow : false,
-                      music : true,
-                      games :false,
-                      setting : false,
-                  })
-                }
-                else if(counter_clock>=10 && counter_clock<15){
-                  self.setState({
-                      coverflow : false,
-                      music : false,
-                      games :true,
-                      setting : false,
-                  })
-                }
-                else if(counter_clock>=15 && counter_clock<20){
-                  self.setState({
-                    coverflow : false,
-                    music : false,
-                    games :false,
-                    setting : true,
-                })
-                }    
-                
-                if(counter_clock>20){
-                  counter_clock=20;
-                }
-                else{
-                  counter_clock = counter_clock+1;
-                }
-            }
+    activeRegion.bind(target, 'rotate', function (event) {
       
-      
-            else if(angle<0){
-      
-      
-              if(counter_clock>0 && counter_clock<=5){
-                self.setState({
-                    coverflow : true,
-                    music : false,
-                    games :false,
-                    setting : false,
-              })
-            }
-            else if(counter_clock>5 && counter_clock<=10){
-              self.setState({
-                  coverflow : false,
-                  music : true,
-                  games :false,
-                  setting : false,
-              })
-            }
-            else if(counter_clock>10 && counter_clock<=15){
-              self.setState({
-                  coverflow : false,
-                  music : false,
-                  games :true,
-                  setting : false,
-              })
-            }
-            else if(counter_clock>15 && counter_clock<=20){
-              self.setState({
-                coverflow : false,
-                music : false,
-                games :false,
-                setting : true,
-              })
-            } 
-            
-            if(counter_clock<0){
-              counter_clock = 0;
-            }
-            else{
-              counter_clock = counter_clock-1;
-            }
-      
+
+      var angle = event.detail.distanceFromLast;
+
+      if (self.state.musiclist==false) {
+        if (angle < 0) {
+         
+          if (counter > 0 && counter <= 5) {
+            self.setState({
+              coverflow: true,
+              music: false,
+              games: false,
+              setting: false
+            })
           }
-      }
+          else if (counter > 5 && counter <= 10) {
+            self.setState({
+              coverflow: false,
+              music: true,
+              games: false,
+              setting: false
+            })
+          }
+          else if (counter > 10 && counter <= 15) {
+            self.setState({
+              coverflow: false,
+              music: false,
+              games: true,
+              setting: false
+            })
+          }
+          else if (counter > 15 && counter <= 20) {
+            self.setState({
+              coverflow: false,
+              music: false,
+              games: false,
+              setting: true
+            })
+          }
 
+          if (counter < 0) { counter = 0 }
+          else { counter = counter - 1; }
 
-      else{
-          if(angle>0){
-            if(counter2>=0 && counter2<5){
-                self.setState({
-                    artist : true,
-                    songs : false,
-              })
-            }
-            else if(counter2>=5 && counter2<10){
-              self.setState({
-                  artist : false,
-                  songs : true,
-              })
-            }
-
-            if(counter2>10){
-              counter2=10;
-            }
-            else{
-              counter2 = counter2+1;
-            }
         }
-        else if(angle<0){
-              if(counter2>0 && counter2<=5){
-                self.setState({
-                    artist : true,
-                    songs : false,
-              })
-            }
-            else if(counter2>5 && counter2<=10){
-              self.setState({
-                  artist : false,
-                  songs : true,
-              })
-            }
 
-            if(counter2<0){
-              counter2 = 0;
-            }
-            else{
-              counter2 = counter2-1;
-            }
+        else if (angle > 0) {
+         
+          if (counter >= 0 && counter < 5) {
+            self.setState({
+              coverflow: true,
+              music: false,
+              games: false,
+              setting: false
+            })
+          }
+          else if (counter >= 5 && counter < 10) {
+            self.setState({
+              coverflow: false,
+              music: true,
+              games: false,
+              setting: false
+            })
+          }
+          else if (counter >= 10 && counter < 15) {
+            self.setState({
+              coverflow: false,
+              music: false,
+              games: true,
+              setting: false
+            })
+          }
+          else if (counter >= 15 && counter < 20) {
+            self.setState({
+              coverflow: false,
+              music: false,
+              games: false,
+              setting: true
+            })
+          }
+
+          if (counter > 20) { counter = 20 }
+          else { counter = counter + 1; }
+
         }
       }
+// *********************************************************
+      else if (self.state.musiclist === true) {
+        
+        
+        if (angle < 0) {
+          
+          if (counter2 > 0 && counter2 <= 5) {
+            self.setState({
+              artist: true,
+              songs: false
+            })
+          }
+          else if (counter2 > 5 && counter2 <= 10) {
+            self.setState({
+              artist: false,
+              songs: true
+            })
+          }
 
+          if (counter2 < 0) { counter2 = 0 }
+          else { counter2 = counter2 - 1; }
+        }
+
+        else if (angle > 0) {
+          
+          if (counter2 >= 0 && counter2 < 5) {
+            self.setState({
+              artist: true,
+              songs: false
+            })
+          }
+          else if (counter2 >= 5 && counter2 < 10) {
+            self.setState({
+              artist: false,
+              songs: true
+            })
+          }
+
+          if (counter2 > 10) { counter2 = 10 }
+          else { counter2 = counter2 + 1; }
+
+        }
+      }
     });
-
 }
 
   centerClick = () => {
