043184216237b7910659241744bf62716f4ebf09
harshv9988 <harshv9988@gmail.com>
2020-10-28 13:23:51 +0530

    removed mouseover added mouse enter

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import React from 'react';
-import Wheel from './components/Wheel';
-import Screen from './components/Screen';
-import MusicScreen from './components/MusicScreen';
-import CoverflowScreen from './components/CoverflowScreen';
-import GamesScreen from './components/GamesScreen';
-import SettingScreen from './components/SettingScreen';
-import ZingTouch from 'zingtouch';
-import song from './assets/songs/Imagine-Dragons-Birds.mp3'
+import React from "react";
+import Wheel from "./components/Wheel";
+import Screen from "./components/Screen";
+import MusicScreen from "./components/MusicScreen";
+import CoverflowScreen from "./components/CoverflowScreen";
+import GamesScreen from "./components/GamesScreen";
+import SettingScreen from "./components/SettingScreen";
+import ZingTouch from "zingtouch";
+import song from "./assets/songs/Imagine-Dragons-Birds.mp3";
 
-class App extends React.Component  {
-
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
 
-    this.state={
+    this.state = {
+      coverflow: true,
+      music: false,
+      games: false,
+      setting: false,
 
-      coverflow : true,
-      music : false,
-      games :false,
-      setting : false,
+      artist: true,
+      songs: false,
 
-      artist : true,
-      songs : false,
+      musiclist: false,
+      musicscreen: false,
+      gamesscreen: false,
+      settingscreen: false,
+      coverflowscreen: false,
 
-      musiclist : false,
-      musicscreen : false,
-      gamesscreen : false,
-      settingscreen : false,
-      coverflowscreen : false,
+      play: false,
+      insidemusic: false,
+    };
 
-      
-      play : false,
-      insidemusic : false,
-
-    }
-
-    this.audio = new Audio(song)
-
+    this.audio = new Audio(song);
   }
 
   rotateClick = () => {
@@ -44,280 +39,256 @@ class App extends React.Component  {
     var counter2 = 0;
     const self = this;
 
-    var target = document.getElementById('rotatediv');
+    var target = document.getElementById("rotatediv");
     var activeRegion = ZingTouch.Region(target);
 
-    activeRegion.bind(target, 'rotate', function (event) {
-      
-
+    activeRegion.bind(target, "rotate", function (event) {
       var angle = event.detail.distanceFromLast;
 
-      if (self.state.musiclist==false) {
+      if (self.state.musiclist == false) {
         if (angle < 0) {
-         
           if (counter > 0 && counter <= 5) {
             self.setState({
               coverflow: true,
               music: false,
               games: false,
-              setting: false
-            })
-          }
-          else if (counter > 5 && counter <= 10) {
+              setting: false,
+            });
+          } else if (counter > 5 && counter <= 10) {
             self.setState({
               coverflow: false,
               music: true,
               games: false,
-              setting: false
-            })
-          }
-          else if (counter > 10 && counter <= 15) {
+              setting: false,
+            });
+          } else if (counter > 10 && counter <= 15) {
             self.setState({
               coverflow: false,
               music: false,
               games: true,
-              setting: false
-            })
-          }
-          else if (counter > 15 && counter <= 20) {
+              setting: false,
+            });
+          } else if (counter > 15 && counter <= 20) {
             self.setState({
               coverflow: false,
               music: false,
               games: false,
-              setting: true
-            })
+              setting: true,
+            });
           }
 
-          if (counter < 0) { counter = 0 }
-          else { counter = counter - 1; }
-
-        }
-
-        else if (angle > 0) {
-         
+          if (counter < 0) {
+            counter = 0;
+          } else {
+            counter = counter - 1;
+          }
+        } else if (angle > 0) {
           if (counter >= 0 && counter < 5) {
             self.setState({
               coverflow: true,
               music: false,
               games: false,
-              setting: false
-            })
-          }
-          else if (counter >= 5 && counter < 10) {
+              setting: false,
+            });
+          } else if (counter >= 5 && counter < 10) {
             self.setState({
               coverflow: false,
               music: true,
               games: false,
-              setting: false
-            })
-          }
-          else if (counter >= 10 && counter < 15) {
+              setting: false,
+            });
+          } else if (counter >= 10 && counter < 15) {
             self.setState({
               coverflow: false,
               music: false,
               games: true,
-              setting: false
-            })
-          }
-          else if (counter >= 15 && counter < 20) {
+              setting: false,
+            });
+          } else if (counter >= 15 && counter < 20) {
             self.setState({
               coverflow: false,
               music: false,
               games: false,
-              setting: true
-            })
+              setting: true,
+            });
           }
 
-          if (counter > 20) { counter = 20 }
-          else { counter = counter + 1; }
-
+          if (counter > 20) {
+            counter = 20;
+          } else {
+            counter = counter + 1;
+          }
         }
       }
-// *********************************************************
+      // *********************************************************
       else if (self.state.musiclist === true) {
-        
-        
         if (angle < 0) {
-          
           if (counter2 > 0 && counter2 <= 5) {
             self.setState({
               artist: true,
-              songs: false
-            })
-          }
-          else if (counter2 > 5 && counter2 <= 10) {
+              songs: false,
+            });
+          } else if (counter2 > 5 && counter2 <= 10) {
             self.setState({
               artist: false,
-              songs: true
-            })
+              songs: true,
+            });
           }
 
-          if (counter2 < 0) { counter2 = 0 }
-          else { counter2 = counter2 - 1; }
-        }
-
-        else if (angle > 0) {
-          
+          if (counter2 < 0) {
+            counter2 = 0;
+          } else {
+            counter2 = counter2 - 1;
+          }
+        } else if (angle > 0) {
           if (counter2 >= 0 && counter2 < 5) {
             self.setState({
               artist: true,
-              songs: false
-            })
-          }
-          else if (counter2 >= 5 && counter2 < 10) {
+              songs: false,
+            });
+          } else if (counter2 >= 5 && counter2 < 10) {
             self.setState({
               artist: false,
-              songs: true
-            })
+              songs: true,
+            });
           }
 
-          if (counter2 > 10) { counter2 = 10 }
-          else { counter2 = counter2 + 1; }
-
+          if (counter2 > 10) {
+            counter2 = 10;
+          } else {
+            counter2 = counter2 + 1;
+          }
         }
       }
     });
-}
+  };
 
   centerClick = () => {
-
-    
-   
-    if(this.state.music && this.state.musiclist===false){
+    if (this.state.music && this.state.musiclist === false) {
       this.setState({
-        musiclist : true,
-      })
+        musiclist: true,
+      });
     }
 
-    if(this.state.musiclist && this.state.songs){
+    if (this.state.musiclist && this.state.songs) {
       this.setState({
-        musicscreen : true
-      })
-      if(this.state.insidemusic){
+        musicscreen: true,
+      });
+      if (this.state.insidemusic) {
         this.state.play = false;
       }
       this.state.insidemusic = true;
       this.playPause();
-    }
-    else if(this.state.musiclist===false && this.state.coverflow){
+    } else if (this.state.musiclist === false && this.state.coverflow) {
       this.setState({
-        coverflowscreen : true
-      })
-    }
-    else if(this.state.musiclist===false && this.state.games){
+        coverflowscreen: true,
+      });
+    } else if (this.state.musiclist === false && this.state.games) {
       this.setState({
-        gamesscreen : true
-      })
-    }
-    else if(this.state.musiclist===false && this.state.setting){
+        gamesscreen: true,
+      });
+    } else if (this.state.musiclist === false && this.state.setting) {
       this.setState({
-        settingscreen : true
-      })
+        settingscreen: true,
+      });
     }
-  }
-
+  };
 
   showMenu = () => {
-    if(this.state.musiclist && this.state.musicscreen){
+    if (this.state.musiclist && this.state.musicscreen) {
       this.setState({
-        musicscreen : false,
-      })
-    }
-    else if(this.state.musiclist){
+        musicscreen: false,
+      });
+    } else if (this.state.musiclist) {
       this.setState({
-        musiclist : false
-      })
-    }
-
-    if(this.state.coverflowscreen){
-      this.setState({
-        coverflowscreen : false
-      })
+        musiclist: false,
+      });
     }
 
-    if(this.state.gamesscreen){
+    if (this.state.coverflowscreen) {
       this.setState({
-        gamesscreen : false
-      })
+        coverflowscreen: false,
+      });
     }
 
-    if(this.state.settingscreen){
+    if (this.state.gamesscreen) {
       this.setState({
-        settingscreen : false
-      })
+        gamesscreen: false,
+      });
     }
 
-  }
+    if (this.state.settingscreen) {
+      this.setState({
+        settingscreen: false,
+      });
+    }
+  };
 
-
-  playPause =() => {
-
-    if(this.state.insidemusic){
-      if(this.state.play==false){
+  playPause = () => {
+    if (this.state.insidemusic) {
+      if (this.state.play == false) {
         this.setState({
           play: true,
         });
         this.audio.play();
-      }
-      else if(this.state.play==true){
+      } else if (this.state.play == true) {
         this.setState({
           play: false,
         });
         this.audio.pause();
       }
-  
     }
-  }
+  };
 
- render(){
-   const {coverflow, music, games, setting, musiclist, artist, songs, musicscreen, coverflowscreen, gamesscreen, settingscreen} = this.state;
-  return (
-    <div className="App">
+  render() {
+    const {
+      coverflow,
+      music,
+      games,
+      setting,
+      musiclist,
+      artist,
+      songs,
+      musicscreen,
+      coverflowscreen,
+      gamesscreen,
+      settingscreen,
+    } = this.state;
+    return (
+      <div className="App">
+        <div className="structure">
+          <div className="body">
+            <Screen
+              coverflow={coverflow}
+              music={music}
+              games={games}
+              setting={setting}
+              musiclist={musiclist}
+              artist={artist}
+              songs={songs}
+              musicscreen={musicscreen}
+              coverflowscreen={coverflowscreen}
+              gamesscreen={gamesscreen}
+              settingscreen={settingscreen}
+            />
 
-     <div className="structure">
+            <MusicScreen musicscreen={musicscreen} />
 
-     <div className="body">
-          <Screen
-          coverflow = {coverflow}
-          music = {music}
-          games = {games}
-          setting = {setting}
-          musiclist = {musiclist}
-          artist = {artist}
-          songs = {songs}
-          musicscreen = {musicscreen}
-          coverflowscreen = {coverflowscreen}
-          gamesscreen = {gamesscreen}
-          settingscreen = {settingscreen}
-          />
+            <CoverflowScreen coverflowscreen={coverflowscreen} />
+            <GamesScreen gamesscreen={gamesscreen} />
+            <SettingScreen settingscreen={settingscreen} />
 
-          <MusicScreen
-            musicscreen = {musicscreen}
-          />
-
-          <CoverflowScreen
-            coverflowscreen = {coverflowscreen}
-          />
-          <GamesScreen
-            gamesscreen = {gamesscreen}
-          />
-          <SettingScreen
-            settingscreen = {settingscreen}
-          />
-
-          <Wheel
-            clickRotate = {this.rotateClick}
-            centerClick = {this.centerClick}
-            showMenu = {this.showMenu}
-            playPause = {this.playPause}
-          />
+            <Wheel
+              clickRotate={this.rotateClick}
+              centerClick={this.centerClick}
+              showMenu={this.showMenu}
+              playPause={this.playPause}
+            />
+          </div>
+        </div>
       </div>
-
-     </div>
-      
-    </div>
-  );
- }
+    );
+  }
 }
 
 export default App;
--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import menu from '../assets/menu.svg'
-import fastforward from '../assets/fast-forward.svg'
-import fastbackward from '../assets/fast-backward.svg'
-import playpause from '../assets/playpause.svg'
+import React from "react";
+import menu from "../assets/menu.svg";
+import fastforward from "../assets/fast-forward.svg";
+import fastbackward from "../assets/fast-backward.svg";
+import playpause from "../assets/playpause.svg";
 
 const Wheel = (props) => {
-    const {clickRotate, centerClick, showMenu, playPause} = props;
+  const { clickRotate, centerClick, showMenu, playPause } = props;
 
-    return (
-        <div className="wheel" onMouseOver = {clickRotate} id="rotatediv">
-            <div className="wheel-button">
-                <img src={menu} alt="Menu" className="menu-image"  onClick={showMenu}/>
-                <img src={fastforward} className="fastforward-image"/>
-                <img src={fastbackward} className="fastbackward-image"/>
-                <img src={playpause} className="playpause-image" onClick={playPause}/>
-                <div className="center-button" onClick={centerClick}>
-
-                </div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="wheel" onMouseEnter={clickRotate} id="rotatediv">
+      <div className="wheel-button">
+        <img src={menu} alt="Menu" className="menu-image" onClick={showMenu} />
+        <img src={fastforward} className="fastforward-image" />
+        <img src={fastbackward} className="fastbackward-image" />
+        <img src={playpause} className="playpause-image" onClick={playPause} />
+        <div className="center-button" onClick={centerClick}></div>
+      </div>
+    </div>
+  );
+};
 
 export default Wheel;
