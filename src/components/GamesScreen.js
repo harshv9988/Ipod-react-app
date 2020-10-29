2f35dbcde5be479323645c9b63072e6e04c840af
harshv9988 <harshv9988@gmail.com>
2020-10-29 13:24:41 +0530

    added games screen

--- src/components/GamesScreen.js
+++ src/components/GamesScreen.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
+import gameimage from "../assets/ipod-games.jpg";
 
 const GamesScreen = (props) => {
-    
-    const{gamesscreen} = props;
-    return(
-
-        <div className={`${gamesscreen ? "games-screen" : "hide"}`}>
-          games
-        </div>
-
-    );
-
-
-}
+  const { gamesscreen } = props;
+  return (
+    <div className={`${gamesscreen ? "games-screen" : "hide"}`}>
+      <div className="display-screen">
+        <img src={gameimage} alt="game image" />
+      </div>
+    </div>
+  );
+};
 export default GamesScreen;
