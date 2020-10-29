fdbd7a14b16166a0e9da62ab332636ba2e3e80a3
harshv9988 <harshv9988@gmail.com>
2020-10-29 14:22:50 +0530

    added flex for split screen

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,38 +1,47 @@
-import React from 'react';
+import React from "react";
 
-const Screen = (props) =>{
-    const {coverflow, music, games, setting, musiclist, artist, songs, musicscreen, coverflowscreen, gamesscreen, settingscreen} = props;
-    return (
-        <div className={`${musicscreen || coverflowscreen || gamesscreen || settingscreen ? "hide" : "screen"}`}>
-            <div className="display-screen">
-                <div className={`${musiclist ? "hide" : "list-container"}`}>
-                    <div className="title">
-                        IPOD.js
-                    </div>
-                    <div className={`${coverflow ? "active" : "coverflow"}`}>
-                        coverflow
-                    </div>
-                    <div className={`${music ? "active" : "music"}`}>
-                        Music
-                    </div>
-                    <div className={`${games ? "active" : "games"}`}>
-                        Games
-                    </div>
-                    <div className={`${setting ? "active" : "setting"}`}>
-                        Setting
-                    </div>
-                </div>
-                <div className={`${musiclist ? "list-container" : "hide"}`}>
-                    <div className={`${artist ? "active" : "artist"}`}>
-                        Artist
-                    </div>
-                    <div className={`${songs ? "active" : "songs"}`}>
-                        Songs
-                    </div>
-                </div>
+const Screen = (props) => {
+  const {
+    coverflow,
+    music,
+    games,
+    setting,
+    musiclist,
+    artist,
+    songs,
+    musicscreen,
+    coverflowscreen,
+    gamesscreen,
+    settingscreen,
+  } = props;
+  return (
+    <div
+      className={`${
+        musicscreen || coverflowscreen || gamesscreen || settingscreen
+          ? "hide"
+          : "screen"
+      }`}
+    >
+      <div className="display-flex-screen">
+        <div className="child-1">
+          <div className={`${musiclist ? "hide" : "list-container"}`}>
+            <div className="title">IPOD.js</div>
+            <div className={`${coverflow ? "active" : "coverflow"}`}>
+              coverflow
             </div>
+            <div className={`${music ? "active" : "music"}`}>Music</div>
+            <div className={`${games ? "active" : "games"}`}>Games</div>
+            <div className={`${setting ? "active" : "setting"}`}>Setting</div>
+          </div>
+          <div className={`${musiclist ? "list-container" : "hide"}`}>
+            <div className={`${artist ? "active" : "artist"}`}>Artist</div>
+            <div className={`${songs ? "active" : "songs"}`}>Songs</div>
+          </div>
         </div>
-    );
-}
+        <div className="child-2">hello</div>
+      </div>
+    </div>
+  );
+};
 
 export default Screen;
