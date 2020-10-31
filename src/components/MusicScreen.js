96a48ebfb8727aab893063063fb96285914f0e2c
harshv9988 <harshv9988@gmail.com>
2020-10-31 12:21:12 +0530

    added artist and song name in music screen

--- src/components/MusicScreen.js
+++ src/components/MusicScreen.js
@@ -12,7 +12,12 @@ const MusicScreen = (props) => {
           <div className="musicchild-1">
             <img src={songposter} alt="songposter" />
           </div>
-          <div className="musicchild-2">BIRDS</div>
+          <div className="musicchild-2">
+            <h3>BIRDS</h3>
+            <h4>BIRDS</h4>
+            <h4>Composed By</h4>
+            <h4>Imagine Dragons</h4>
+          </div>
         </div>
       </div>
     </div>
